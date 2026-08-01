d9ea39035aad78aec8722b34d893ef7c8f5c3729
SEVEN-8\user <nonore1111@gmail.com>
2026-08-01 16:58:47 +0800

    Start empty amount fields

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qingzhang-v15';
+const CACHE_NAME = 'qingzhang-v16';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon.svg'];
 
 self.addEventListener('install', event => {

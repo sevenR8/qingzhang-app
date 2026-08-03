4100d2931a9c6ae4471c72e5ce33565bfef1f9d6
SEVEN-8\user <nonore1111@gmail.com>
2026-08-03 19:46:02 +0800

    Add mobile month swipe feedback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qingzhang-v43';
+const CACHE_NAME = 'qingzhang-v44';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/icon.svg'];
 
 self.addEventListener('install', event => {
